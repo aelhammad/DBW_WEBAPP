8bb37ae43031f0f869346997e20702263f87bca7
Allal El Hommad <allalelhommad@mar-101-74.eduroamcm.upf.edu>
2024-02-19 16:14:44 +0100

    New tables

--- model.py
+++ model.py
@@ -19,7 +19,7 @@ compound_name, chemical_formula, canonical_smiles, isomeric_smiles, molecular_we
 type_names = json.dumps(type_names)
 pictogram_urls = json.dumps(pictogram_urls)
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///toxicbrowser'  # Update with your database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///toxicbrowser.db'  # Update with your database URI
 db = SQLAlchemy(app)
 
 user_entries = db.Table('user_entries',
@@ -46,17 +46,20 @@ class Entry(db.Model):
     pubchem_id = db.Column(db.String(10), nullable=False)
     chembl_id = db.Column(db.String(10), nullable=False)
     inchi_key = db.Column(db.String(30), nullable=False)
-    chemical_formula = db.Column(db.String, nullable=False) # from pubchem
-    compound_name = db.Column(db.String(50), nullable=False) # from pubchem
-    molecular_weight = db.Column(db.Float) # from pubchem
-    type_names = db.Column(db.String(40)) # type of compound: antago, agonist, etc
-    description = db.Column(db.String(length=None)) # from json and pubchem
-    description1 = db.Column(db.String(length = None)) # from json and pubchem
-    canonical_smiles = db.Column(db.String(200))  # Column for canonical SMILES, pubchem
-    isomeric_smiles = db.Column(db.String(200))  # Column for isomeric SMILES, pubchem 
-    mechanism_of_toxicity = db.Column(db.String(200))  # from td3
-    treatment = db.Column(db.String(200))  # from td3
+    chemical_formula = db.Column(db.String, nullable=False)
+    compound_name = db.Column(db.String(50), nullable=False)
+    molecular_weight = db.Column(db.Float)
+    description = db.Column(db.String(length=None))
+    description1 = db.Column(db.String(length=None))
+    canonical_smiles = db.Column(db.String(200))
+    isomeric_smiles = db.Column(db.String(200))
+    mechanism_of_toxicity = db.Column(db.String(200))
+    treatment = db.Column(db.String(200))
     year_id = db.Column(db.Integer, db.ForeignKey('year.id'))  # Foreign key reference to Year table
+    year = db.relationship('Year', backref='entry')  # Relationship with Year table
+    pictograms = db.relationship('Pictograms', backref='entry')
+    typename = db.relationship('TypeName', backref='entry')
+
 
 
 class Health_effects(db.Model):
@@ -65,9 +68,20 @@ class Health_effects(db.Model):
     health_effects = db.Column(db.String(200))
 
 class Year(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    creation_year = db.Column(db.Integer)
+
+
+class Pictograms(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
-    creation_year = db.Column(db.Integer, nullable=False)
-    entries = db.relationship('Entry', backref='year')
+    entry_id = db.Column(db.Integer, db.ForeignKey('entry.id'), nullable=False)
+    pictogram_urls = db.Column(db.String(200))
+
+class TypeName(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    entry_id = db.Column(db.Integer, db.ForeignKey('entry.id'), nullable=False)
+    type_names = db.Column(db.String(200))
+    
 
 if __name__ == '__main__':
     with app.app_context():
@@ -82,7 +96,6 @@ if __name__ == '__main__':
             chemical_formula=chemical_formula,
             compound_name=compound_name,
             molecular_weight=molecular_weight,
-            type_names=type_names,
             description=description,
             description1=description1,
             canonical_smiles=canonical_smiles,
@@ -97,5 +110,13 @@ if __name__ == '__main__':
 
         benzene_health_effects = Health_effects(entry_id=benzene_entry.id, health_effects=health_effects)
         db.session.add(benzene_health_effects)
+
+        benzene_pictograms = Pictograms(entry_id=benzene_entry.id, pictogram_urls=pictogram_urls)
+        db.session.add(benzene_pictograms)
+
+        benzene_type_names = TypeName(entry_id=benzene_entry.id, type_names=type_names)
+        db.session.add(benzene_type_names)
+
+
 
         db.session.commit()
